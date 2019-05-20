b55434d432a58fdfeb9e6f9fd39f51db9fbd64bd
Gabriel Matheus <33676582+gmsj@users.noreply.github.com>
2019-05-20 17:12:48 -0300

    Create sinTaylor.c

---
+++ sinTaylor.c
@@ -0,0 +1,48 @@
+/************************************************************
+*                                                           *
+*                Exemplo de utilização de FPU               *    
+*                  e integração C e assembly		        *
+*                   Autor: Gabriel Matheus                  *    
+*                       github.com/gmsj                     *
+*                                                           *
+************************************************************/
+// Parte complementar em C
+
+#include <stdio.h>
+#include <stdlib.h>
+
+float obterAngulo () {
+	float angulo;
+	printf("Digite o angulo:\n");
+	scanf("%f", &angulo);
+	return angulo;
+}
+
+float obterDif () {
+	float diferenca;
+	printf("Digite a diferenca:\n");
+	scanf("%f", &diferenca);
+	return diferenca;
+}
+
+void imprimir (float seno, float interacoes) {
+	printf("Valor do seno [%f] - Num de interacoes [%.0f]\n", seno, interacoes);
+}
+
+float fatorial (float num) {
+	if (num > 1)
+		num *= fatorial(num-1);
+	return num;
+}
+
+float pot (float num, float exp) {
+	int i;
+	float result = 1;
+	if (exp == 0) {
+		return 1;
+	}
+	for (i = 0; i < exp; i++) {
+		result = result * num;
+	}
+	return result;
+}
